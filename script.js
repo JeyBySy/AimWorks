ba86f0578ce2ae1108ffd538842554123c069743
JeyBySy <njcocosa@gmail.com>
2024-02-07 01:57:59 +0800

    Initial commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const screens = document.querySelectorAll('.screen');
 const start_btn = document.getElementById('start-btn')
 const chooseCategory = document.querySelectorAll('.choose-cat');
+const highscoreDisplay = document.getElementById('screenHighscore')
+
 start_btn.addEventListener('click', () => screens[0].classList.add('up'))
 
 chooseCategory.forEach(btn => {
     btn.addEventListener('click', () => {
         screens[1].classList.add('up')
+        highscoreDisplay.style.visibility = 'visible'
+        document.body.style.cursor = 'crosshair'
     })
 })
